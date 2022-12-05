a4e7135dff0a80a83e209af19c98a8eb1ac0d0c2
Muskan Valmikee <muskanvalmikee@gmail.com>
2022-12-05 22:17:43 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <table id='table'>
+        <th id='td1'>Name</th>
+        <th id='td1'>Team Members</th>
+        <tr>
+          <td id='td'>Muskan</td>
+          <td id='td'>Natwest</td>
+        </tr>
+        <tr>
+          <td id='td'>Gudia</td>
+          <td id='td'>Natwest</td>
+        </tr>
+        <tr>
+          <td id='td'>Ishita</td>
+          <td id='td'>Natwest</td>
+        </tr>
+        <tr>
+          <td id='td'>Aishani</td>
+          <td id='td'>Natwest</td>
+        </tr>
+        <tr>
+          <td id='td'>Manori</td>
+          <td id='td'>Natwest</td>
+        </tr>
+        <tr>
+          <td id='td'>Shubhadra</td>
+          <td id='td'>Natwest</td>
+        </tr>
+        <tr>
+          <td id='td'>Trupti</td>
+          <td id='td'>Natwest</td>
+        </tr>
+        <tr>
+          <td id='td'>Deepshika</td>
+          <td id='td'>Natwest</td>
+        </tr>
+        <tr>
+          <td id='td'>Anusha</td>
+          <td id='td'>Natwest</td>
+        </tr>
+        <tr>
+          <td id='td'>simran</td>
+          <td id='td'>Natwest</td>
+        </tr>
+        <tr>
+          <td id='td'>Jyoti</td>
+          <td id='td'>Natwest</td>
+        </tr>
+        <tr>
+          <td id='td'>Prachi</td>
+          <td id='td'>Natwest</td>
+        </tr>
+        <tr>
+          <td id='td'>Aarti</td>
+          <td id='td'>Natwest</td>
+        </tr>
+        <tr>
+          <td id='td'>Sheetal</td>
+          <td id='td'>Natwest</td>
+        </tr>
+        <tr>
+          <td id='td'>Shivani</td>
+          <td id='td'>Natwest</td>
+        </tr>
+      </table>
+    </div>
+  )
+}
+export default App;
